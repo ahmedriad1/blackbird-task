7957b4cd15e5037dc3ad471c4d7c53d301f9af25
Ahmed Riad <ahmed.riad.elashrey@gmail.com>
2022-09-04 06:32:52 +0300

    update logo and link

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <div className='App'>
+      <header className='App-header'>
+        <img
+          src='https://www.propelleraero.com/wp-content/uploads/2021/05/Vector.svg'
+          className='App-logo'
+          alt='logo'
+        />
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://www.propelleraero.com/dirtmate/'
+          target='_blank'
+          rel='noopener noreferrer'
         >
-          Learn React
+          https://www.propelleraero.com/dirtmate/
         </a>
       </header>
     </div>
   );
-}
+};
 
 export default App;
